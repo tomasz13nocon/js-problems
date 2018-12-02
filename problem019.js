504c7fd8d842b35ba6fd6e9f9e1cc303b41ad27a
qwe <qwe>
2018-12-02 22:45:12 +0100

    qwe

--- problem019.js
+++ problem019.js
@@ -1,3 +1,14 @@
 function carry(a) {
-
+	var args = [];
+	var i = a.length;
+	return function(x) {
+		args.push(x);
+		if (i-- > 1) {
+			return arguments.callee;
+		}
+		console.log(args);
+		return a(...args);
+	}
 }
+
+console.log(carry((a,b) => a-b)(2)(1));
